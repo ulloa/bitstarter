a6d64495f849058bb0d74c8eb3734581e536df0d
Samuel Ulloa <samuel.ulloa@gmail.com>
2013-07-30 05:27:28 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
+var buf = new Buffer(256);
+
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync(index.html));
+  response.send(buf.readFileSync(index.html));
 });
 
 var port = process.env.PORT || 5000;
