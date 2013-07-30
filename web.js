5f8fa47b6dee26f80459ce362a493f9351c08b56
Samuel Ulloa <samuel.ulloa@gmail.com>
2013-07-30 04:59:01 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(index.html.toString('utf-8'));
+  response.send(fs.readFileSync(index.html));
 });
 
 var port = process.env.PORT || 5000;
