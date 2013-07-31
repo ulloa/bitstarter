9bc60e3f73db335d2a1038cb30071ed828809d46
Samuel Ulloa <samuel.ulloa@gmail.com>
2013-07-31 10:36:53 +0000

    updated grader.js

--- grader.js
+++ grader.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env mode
+#!/usr/bin/env node
 /*
 Automatically grade files for the presence of specified HTML tags attributes
 .
@@ -26,14 +26,18 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
+var htmlfile = cheerio.load('<ul href="infile">...</ul>')
 
-var assertFileExist = function(infile) {
+var assertFileExists = function(infile) {
     var instr = infile.toString();
+    rest.get(instr).on('complete', function(result) {
     if(!fs.existsSync(instr)) {
 	console.log("%s does not exist. Exiting.", instr);
 	process.exit(1); //http://nodejs.org/api/process.html#process_process_exit_code
     }
-    return instr;
+    sys.puts(result);
+    });
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -65,8 +69,9 @@ if(require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'url of html file', clone(assertFileExists), HTMLFILE_DEFAULT)
 	.parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, progam.checks);
+    var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
