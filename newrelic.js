19e215fd9d00daa5c99941de64c5b5fa96a5c87b
ulloa <samuel.ulloa@gmail.com>
2013-10-23 13:48:03 -0700

    newrelic

--- newrelic.js
+++ newrelic.js
@@ -12,7 +12,8 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key : 'b8986a78fc4f77581937a622410e323550aa2c0f',
+  license_key : '07d2a7bb1ed0ee9efde46218f96b8a4b44803090',
+/* for production  license_key : 'b8986a78fc4f77581937a622410e323550aa2c0f', */
   logging : {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
