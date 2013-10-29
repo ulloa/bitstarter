bf38accf1ccf92aa42d493028952b807912518ef
ulloa <samuel.ulloa@gmail.com>
2013-10-28 18:05:19 -0700

    newrelic master

--- newrelic.js
+++ newrelic.js
@@ -12,7 +12,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key : '07d2a7bb1ed0ee9efde46218f96b8a4b44803090',
+  license_key : 'b8986a78fc4f77581937a622410e323550aa2c0f',
 /* for production  license_key : 'b8986a78fc4f77581937a622410e323550aa2c0f', */
   logging : {
     /**
