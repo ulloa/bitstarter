317165bc3244c03f262c9b91c4ce96ff71ef3877
ulloa@gmail.com <samuel.ulloa@gmail.com>
2015-08-21 23:18:43 -0700

    fixed express routing. Added navbar back

--- web.js
+++ web.js
@@ -1,8 +1,13 @@
 require('newrelic');
+var morgan = require('morgan');
 var express = require('express');
 var app = express();
-app.use(express.logger());
+var router = express.Router();
+var path    = require("path");
+app.use(morgan('combined'));
 app.use("/public", express.static(__dirname + '/public'));
+
+
 
 var fs = require('fs');
 var htmlfile = "index.html";
@@ -13,6 +18,13 @@ app.get('/', function(request, response) {
 //  response.send(fs.readFileSync('index.html').tostring());
 });
 
+app.get('/projects', function(req, res, next) {
+    var html = '/public/projects.html';
+    res.sendFile(path.join(__dirname + html));
+})
+
+
+//app.use('/', router);
 app.use(express.static(__dirname + '/public')); // just added for static files
 
 var port = process.env.PORT || 8080;
